9983046ed89740beaa199e1d99a20f422f2146c2
Mythili <125266589+Mythili2909@users.noreply.github.com>
2026-03-09 20:10:35 +0530

    Time: 5 ms (78.88%) | Memory: 45.8 MB (93.84%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,22 +1,25 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        
         if(x<0)
         {
             return false;
         }
-        int n=x;
         int rev=0;
+        int n=x;
         while(n>0)
         {
             int d=n%10;
             rev=rev*10+d;
             n=n/10;
         }
+
         if(rev==x)
         {
             return true;
+
         }
         return false;
- 
+
     }
 }
